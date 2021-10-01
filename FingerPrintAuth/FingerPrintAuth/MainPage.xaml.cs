d1cabbe7e4795f7b25a17f3ac7f882a71005db40
Muthukumaran-G <muthukumaran.gnanavinayagam@syncfusion.com>
2021-10-01 17:13:57 +0530

    Included demo files

---
+++ FingerPrintAuth/FingerPrintAuth/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FingerPrintAuth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var auth = await GetAuthentication();
+            if (auth)
+            {
+                stack.IsVisible = true;
+            }
+        }
+
+        private async Task<bool> GetAuthentication()
+        {
+            var availability = await CrossFingerprint.Current.GetAvailabilityAsync(true);
+            if (availability == FingerprintAvailability.Available)
+            {
+                var authenticate = await CrossFingerprint.Current.AuthenticateAsync(new AuthenticationRequestConfiguration("Authentication required", "Touch the fingerprint sensor") { AllowAlternativeAuthentication = true });
+
+                if (authenticate.Status == FingerprintAuthenticationResultStatus.Succeeded)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
